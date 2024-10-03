5ef9fbd7e2f0ca3e5061b7509d3deb67747db7db
chamz99 <rasanjaleebmc.19@uom.lk>
2024-10-03 09:55:11 +0530

    Add more update for adbout me page

--- src/Component/About/About.jsx
+++ src/Component/About/About.jsx
@@ -6,26 +6,29 @@ const About = () => {
     <div className='about'>
         <div className='about-title'>
           <h1>About me</h1>
-          <img src=''alt=''/>
           </div>  
-          <div className='about-sections'></div>
-          <div className='about-left'>
-            <img src={aboutmeimage}alt=''/>
-          </div>
-          <div className='about-right'>
-            <div className='about-para'>
-            <p>Hi, I'm Chamudi Rasanjalee, a passionate and motivated software engineer with a strong foundation in 
-              full-stack development and a keen interest in building scalable, user-centric applications. Currently 
-              pursuing a BSc in Information Technology at the University of Moratuwa, I have gained practical experience 
-              through internships and academic projects, honing my skills in technologies like React, .NET, and SQL.
-              I thrive in dynamic environments where I can solve problems and contribute to impactful solutions, 
-              whether it's developing efficient ERP systems or collaborating on innovative team projects. I'm always eager to learn new technologies and tackle challenging problems, aiming to grow both personally and professionally in the ever-evolving field of software engineering.</p>
+          <div className='about-sections'>
+            <div className='about-left'>
+              <img src={aboutmeimage}alt=''/>
             </div>
+            <div className='about-right'>
+              <div className='about-para'>
+              <p>Hi, I'm Chamudi Rasanjalee, a passionate and motivated software engineer with a strong foundation in
+                full-stack development and a keen interest in building scalable, user-centric applications. Currently
+                pursuing a BSc in Information Technology at the University of Moratuwa, I have gained practical experience
+                through internships and academic projects, honing my skills in technologies like React, .NET, and SQL.</p>
+                <p> I'm always eager to learn new technologies and tackle challenging problems, aiming to grow both personally and professionally in 
+                 the ever-evolving field of software engineering.</p>
+              </div>
+              <div className='about-skills'>
+                <div className='about-skill'><p>HTML & CSS</p><hr style={{width:"50%"}}></hr></div>
+                            <div className='about-skill'><p>React Js</p><hr style={{width:"50%"}}></hr></div>
+                            <div className='about-skill'><p>JavaScript</p><hr style={{width:"50%"}}></hr></div>
+                            <div className='about-skill'><p>Java</p><hr style={{width:"50%"}}></hr></div>
+              </div>
+            </div>
+            
           </div>
-          <div className='about-skill'><p>HTML & CSS</p><hr style={{width:"50%"}}></hr></div>
-          <div className='about-skill'><p>React Js</p><hr style={{width:"50%"}}></hr></div>
-          <div className='about-skill'><p>JavaScript</p><hr style={{width:"50%"}}></hr></div>
-          <div className='about-skill'><p>Java</p><hr style={{width:"50%"}}></hr></div>
 
 
     </div>
