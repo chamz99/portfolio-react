ac0a69c89356a014fdaff2c67a2b6cc3ff7c9d5f
Chamudi Rasanjalee <91789995+chamz99@users.noreply.github.com>
2024-10-20 19:46:05 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/https://vercel.com/chamz99s-projects/portfolio-react-chamudi/",
+  base: "./",
 });
